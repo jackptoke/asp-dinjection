67a078a09ec5e38c756510d1e01030444272ca3a
Jack Toke <54049012+jackptoke@users.noreply.github.com>
2022-08-23 04:36:43 +1000

    First stage

---
+++ source/Web/DotNet6.Di.Web.Mvc/Program.cs
@@ -0,0 +1,17 @@
+namespace DotNet6.Di.Web.Mvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
